504a18bf4844d8d1335d2aadc800505808a2ccdc
lucasleitebr <lucas.leite13@fatec.sp.gov.br>
2025-02-03 21:30:53 -0300

    imersao-alura-spotify-final

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const resultArtist = document.getElementById('result-artist');
 const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
     fetch(url)
         .then((response) => response.json())
-        .then((result) => displayResults(result, searchTerm))
+        .then((result) => displayResults(result, searchTerm));
 }
 
 function displayResults(result, searchTerm) {
     resultPlaylist.classList.add('hidden');
     const gridContainer = document.querySelector('.grid-container');
-    gridContainer.innerHTML = '';//Limpa os resultados anteriores
+    gridContainer.innerHTML = ''; // Limpa os resultados anteriores
 
     const filteredArtists = result.filter(artist => artist.name.toLowerCase().includes(searchTerm));
 
@@ -27,24 +27,27 @@ function displayResults(result, searchTerm) {
                     <span class="fa fa-solid fa-play"></span>
                 </div>
             </div>
-                <div class="card-text">
-                    <span class="artist-name">${artist.name}</span>
-                    <span class="artist-categorie">Artista</span>
-                </div>`
-        ;
+            <div class="card-text">
+                <span class="artist-name">${artist.name}</span>
+                <span class="artist-categorie">Artista</span>
+            </div>`;
         gridContainer.appendChild(artistCard);            
     });
+
+    // Exibe ou oculta a seção de resultados com base na quantidade de artistas filtrados
+    if (filteredArtists.length > 0) {
         resultArtist.classList.remove('hidden');
+    } else {
+        resultArtist.classList.add('hidden'); // Oculta se não houver resultados
+    }
 }
 
-document.addEventListener('input', function() {
+searchInput.addEventListener('input', function() {
     const searchTerm = searchInput.value.toLowerCase().trim();
     if (searchTerm === '') {
         resultPlaylist.classList.add('hidden');
-        resultArtist.classList.remove('hidden');
+        resultArtist.classList.add('hidden'); // Oculta a seção de resultados
         return;
     }
-
     requestApi(searchTerm);
 });
-
